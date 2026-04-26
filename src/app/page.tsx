a9e4ecd6863059cc328bbf83f05283006101c835
siam-miya <siammiya2024@gamil.com>
2026-04-27 00:02:39 +0600

    currection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
 
-const HomePage = () => {
-  return (
-    <div>
-      HomePage                                                                                                                                                                                                                                                                                                        
-    </div>
-  )
-}
-export default HomePage
+
 
