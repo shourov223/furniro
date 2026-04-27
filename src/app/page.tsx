4ce245a55a8f3801e057291ecc6a615d70208ff8
lazy-coder420 <mjmunna692@gmail.com>
2026-04-27 18:43:16 +0600

    productcard part add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+// import ComparePage from "@/components/munna/ProductComparison";
+import ComparisonSection from "@/components/munna/ProductCard";
 import Banner from "@/components/shourov/Banner";
 import BrowseTheRange from "@/components/shourov/BrowseTheRange";
 import Explore from "@/components/shourov/Explore";
 import FuniroFurniture from "@/components/shourov/FuniroFurniture";
 import OurProducts from "@/components/shourov/OurProducts";
+
 
 const HomePage = () => {
   return (
@@ -12,6 +15,8 @@ const HomePage = () => {
       <OurProducts />
       <Explore />
       <FuniroFurniture />
+
+      <ComparisonSection />
     </main>
   );
 }
