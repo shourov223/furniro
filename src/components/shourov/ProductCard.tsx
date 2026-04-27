0d13422c4deadd905dbe1b3e6625bdf9bb9404f9
shourov223 <shouravislam201@gmail.com>
2026-04-27 14:40:13 +0600

    error fixed

--- src/components/shourov/ProductCard.tsx
+++ src/components/shourov/ProductCard.tsx
@@ -20,8 +20,8 @@ const ProductCard = ({ img, productName, price, category, oldPrice, discountPerc
         <div className="relative group">
             <div className="absolute z-50 top-0 left-0 min-w-0 h-full bg-black opacity-0 group-hover:opacity-50 group-hover:min-w-full transition-all duration-300">
             </div>
-            <div className="absolute top-[50%] left-[50%] translate-x-[-50%] w-full h-full z-60 opacity-0 group-hover:opacity-100 transition-all duration-450">
-                <div className="w-full h-full">
+            <div className="absolute top-[50%] left-[50%] translate-x-[-50%] w-full h-auto z-60 opacity-0 group-hover:opacity-100 transition-all duration-450">
+                <div className="w-full h-auto">
                     <button className="py-3 px-[52px] bg-white whitespace-nowrap text-[#E89F71] font-semibold cursor-pointer grid place-items-center mb-[20px] block mx-auto">
                         Add to Cart
                     </button>
