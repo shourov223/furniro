7c946672712e49ce4d51334499fc460c01a6069c
Md-Gajjali <hellogajjali@gmail.com>
2026-04-28 11:11:27 +0600

    update cart

--- src/components/Emam/CartMini.tsx
+++ src/components/Emam/CartMini.tsx
@@ -1,35 +1,34 @@
-import Image from 'next/image';
+import TrashIcon from '@/assets/icons/icons_componets/TrashIcon';
+import Image, { StaticImageData } from 'next/image';
 import React from 'react'
 
 export interface CartMini {
     title: string;
     price: number;
     subPrice:number
-    img: string 
+    src: string | StaticImageData
 }
 
 
-const CartMini = ({ title , price , subPrice , img }: CartMini) => {
+const CartMini: React.FC<CartMini> = ({ title , price , subPrice , src }) => {
   return (
-    <div className='flex justify-between items-center'>
-        <div className='flex items-center'>
-            <Image src={img} alt="image" width={108} height={105} className='mr-[34px]'/>
+    <div className='flex  items-center mt-5'>
+        <div className='flex items-center gap-10'>
+            <Image src={src} alt="image" width={108} height={105} className=''/>
             <p>{title}</p>
         </div>
-
-        <div className='ml-[7px]'>
+        <div className=' ml-[130px]'>
             <p>{price}</p>
         </div>
-        <div>
-            <input type="text" />
+        <div className='ml-[140px]'>
+            <input type="text" className='w-[32px] h-[32px] flex  justify-center items-center px-2 border-2 rounded-[4px] '/>
         </div>
-        <div>
+        <div className='ml-[60px] '>
             <p>{subPrice}</p>
         </div>
-        <div>
-            <p>{subPrice}</p>
+        <div className='ml-[90px]'>
+            <TrashIcon />
         </div>
-        
     </div>
   )
 }
