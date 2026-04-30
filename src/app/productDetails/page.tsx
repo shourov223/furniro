71a2f5669348a94381bfc97b6b4866e9fa7cd191
Md-Gajjali <hellogajjali@gmail.com>
2026-04-30 13:18:10 +0600

    update product details

--- src/app/productDetails/page.tsx
+++ src/app/productDetails/page.tsx
@@ -6,8 +6,7 @@ import Rating from "@/components/Emam/Rating"
 import SizeCard from "@/components/Emam/SizeCard"
 import CommonBtn from "@/components/shourov/CommonBtn"
 import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa"
-// import { title } from "process"
-
+import ssofa from "@/assets/images/SSofa.png"
 
 export interface Product {
     title:string;
@@ -16,6 +15,8 @@ export interface Product {
     review:string;
     suku:string;
     Tags:string;
+    des1:string;
+    des2:string;
     Category:string;
     description:string
 }
@@ -26,11 +27,14 @@ const page = ({
     title="Asgaard sofa",
     price= 250000 ,
     rating= 4.6,
+    
     review= "5 Customer Review",
     suku='SS001',
     Category='Sofa',
     Tags='Sofa, Chair, Home, Shop',
-    description="Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound"
+    description="Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound",
+    des1="Embodying the raw, wayward spirit of rock ‘n’ roll, the Kilburn portable active stereo speaker takes the unmistakable look and sound of Marshall, unplugs the chords, and takes the show on the road.",
+    des2="Weighing in under 7 pounds, the Kilburn is a lightweight piece of vintage styled engineering. Setting the bar as one of the loudest speakers in its class, the Kilburn is a compact, stout-hearted hero with a well-balanced audio which boasts a clear midrange and extended highs for a sound that is both articulate and pronounced. The analogue knobs allow you to fine tune the controls to your personal preferences while the guitar-influenced leather strap enables easy and stylish travel.",
 }, Product:Product[]) => {
   return (
     <>
@@ -105,12 +109,30 @@ const page = ({
             </div>
             <hr  className="text-[#9F9F9F] mt-10 w-full"/>
 
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center mt-12">
                 <div className="flex  gap-10">
                     <span  className="text-2xl font-medium  " >Description</span>
                     <span  className="text-2xl font-medium text-[#9F9F9F]  ">Additional Information</span>
                     <span  className="text-2xl font-medium text-[#9F9F9F] ">Reviews {review}</span>
                 </div>
+            </div>
+            <div className="mt-10 flex justify-center items-center ">
+                <div className=" text-start ">
+                    <p className="text-[#9F9F9F] w-[1026px] text-[16px]">{des1}</p>
+                    <p className="mt-[20px] text-[#9F9F9F]  w-[1026px] text-[16px]">{des2}</p>
+                </div>
+            </div>
+
+            <div className="flex gap-[29px] py-15">
+                <Image src={ssofa} width={605} height={338}  alt="image"/>
+                <Image src={ssofa} width={605} height={338}  alt="image"/>
+            </div>
+
+            <div>
+                <div className="flex justify-center items-center">
+                    <h3 className="font-medium text-4xl">Related Products</h3>
+                </div>
+                
             </div>
         </div>
       </section>
